6d2548540cff63fa56e42070365d8c47175e9266
Tobias Holtdirk <tobias.holtdirk@mailbox.org>
2021-12-20 16:58:23 +0100

    Remove MAS API support

--- content/include.js
+++ content/include.js
@@ -1,5 +1,5 @@
 // Only create main object once
 let loader = Components.classes['@mozilla.org/moz/jssubscript-loader;1']
 					.getService(Components.interfaces.mozIJSSubScriptLoader);
-let scripts = ['webpack','MASMetaData', 'masAPIKey'];
+let scripts = ['webpack','MASMetaData'];
 scripts.forEach(s => loader.loadSubScript('chrome://zotero-mas-metadata/content/' + s + '.js'));
